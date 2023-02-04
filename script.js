4efdb8d79903e5d2ce504892cb02ee3e80da72aa
abulhasan7bd <abulhasan7bd@gmail.com>
2023-02-05 00:29:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+for ( x = 1; x <= 100; x++){
+    if(x % 3 == 0){
+      console.log(x,"fizz")
+    } 
+    else if( x % 5 == 0){
+      console.log(x,"buzz")
+    }else{
+      console.log("FizzBuzz")
+    }
+    }     
+   
